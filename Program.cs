6930604419262aebd67409bb5105578115b5afd3
Ricardo Santana <slopes.ricardo@gmail.com>
2020-10-01 21:43:48 -0300

    Jogo Criado

---
+++ Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Adivinhe_um_numero
+{
+    class Program
+    {
+        public static Dictionary<string, int> usuarios = new Dictionary<string, int>();
+        static void Main(string[] args)
+        {            
+            do
+            {
+                int opcao = menuPrincipal();
+
+                if (opcao == 1)
+                {
+                    limpaTela();
+                    jogaAdivinha();
+
+                }
+                else if (opcao == 2)
+                {
+                    limpaTela();
+                    rank();
+                    Console.ReadLine();
+
+                }
+                else if (opcao == 3)
+                {
+                    limpaTela();
+                    sobre();
+
+                }
+                else if (opcao == 4)
+                {
+                    limpaTela();
+                    Console.WriteLine("Até Mais!");
+                    Console.ReadLine();
+                    break;
+                }
+            } while (true);
+ 
+        }
+
+        public static void mesagemPrincipal(string mensagem)
+        {      
+            Console.WriteLine("\n\t*** {0} ***\n", (mensagem));
+        }
+
+        public static void limpaTela()
+        {
+            Console.Clear();
+        }
+
+        public static int menuPrincipal()
+        {
+            int opcao;
+            do
+            {
+                limpaTela();
+                mesagemPrincipal("Bem Vindo ao Adivinha o Número");
+                Console.Write("\tSelecione uma das opções abaixo\n(1) - Jogar\n(2) - Rank\n(3) - Sobre\n(4) - Sair\n: ");
+                opcao = Int32.Parse(Console.ReadLine());
+
+                if (opcao < 1 || opcao > 4)
+                {
+                    Console.WriteLine("Digite apenas valores de 1 a 4!!!");
+                    Console.ReadLine();
+                    limpaTela();
+                }
+            } while (opcao < 1 || opcao > 4);
+
+            return opcao;
+        }
+
+        public static int geraNumeroSecreto()
+        {
+            Random numeroAleatorio = new Random();
+            int numeroSecreto = numeroAleatorio.Next(1, 101);
+            return numeroSecreto;
+        }
+
+        public static int selecionaNivel()
+        {
+            Console.Write("\tEm qual nível deseja jogar?\n\n(1) - Fácil\n(2) - Moderado\n(3) - Díficil\n: ");
+            int nivel = Int32.Parse(Console.ReadLine());
+            int rodadas = 0;
+            switch (nivel)
+            {
+                case 1:
+                    rodadas = 20;
+                    break;
+                case 2:
+                    rodadas = 10;
+                    break;
+                case 3:
+                    rodadas = 5;
+                    break;
+                default:
+                    Console.WriteLine("Entrada Inválida");
+                    break;
+            }
+
+            return rodadas;
+        }
+
+        public static int solicitaChute(int tentativa, int rodadas)
+        {
+            Console.WriteLine("Tentativa " + tentativa + " de " + rodadas);
+            Console.WriteLine("Digite um número inteiro entre de 1 a 100");
+            Console.WriteLine("Qual o seu chute?");
+            int chute = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("\nSeu chute foi: " + chute);
+            return chute;
+        }
+
+        public static void mostraNumeroSecreto(int numeroSecreto)
+        {
+            Console.WriteLine("Número Secreto: " + numeroSecreto);
+        }
+
+        public static int calculaPontuacao(int pontuacao, int chute, int numeroSecreto)
+        {
+            pontuacao = pontuacao - Math.Abs(numeroSecreto - chute);
+            return pontuacao;
+        }
+
+        public static void mostraPontuacao(int pontuacao)
+        {
+            Console.WriteLine("Sua pontuação foi: " + pontuacao);
+        }
+
+        public static void jogaAdivinha()
+        {
+            int rodadas = 0;
+            int chute = 0;
+            int pontuacao = 1000;
+            string nome;
+
+            int numeroSecreto = geraNumeroSecreto();
+            
+            mesagemPrincipal("Adivinha o Número");
+            rodadas = selecionaNivel();
+            limpaTela();
+            Console.WriteLine("Qual o seu nome?");
+            nome = Console.ReadLine();
+            Console.WriteLine("\nVamos jogar " + nome);
+            for (int tentativa = 1; tentativa <= rodadas; tentativa++)
+            {
+                chute = solicitaChute(tentativa, rodadas);
+
+                bool acertou = chute == numeroSecreto;
+                bool maior = chute > numeroSecreto;
+                bool menor = chute < numeroSecreto;
+
+                if (acertou)
+                {
+                    Console.WriteLine("Você acertou!");
+                    pontuacao = pontuacao + 50;
+                    break;
+                }
+                else
+                {
+                    if (maior)
+                    {
+                        Console.WriteLine("Seu chute foi maior que o número secreto.\n");
+                    }
+                    else if (menor)
+                    {
+                        Console.WriteLine("Seu chute foi menor que o número secreto.\n");
+                    }
+                }
+
+                pontuacao = calculaPontuacao(pontuacao, chute, numeroSecreto);
+            }
+
+            mostraNumeroSecreto(numeroSecreto);
+            mostraPontuacao(pontuacao);
+
+            usuarios.Add(nome, pontuacao);
+
+            Console.ReadLine();
+        }
+
+        public static void rank()
+        {
+            mesagemPrincipal("Rank");
+            foreach(KeyValuePair<string, int> usuario in usuarios.OrderByDescending(key => key.Value))
+            {
+                Console.WriteLine("Nome = {0}   Pontuação = {1}", usuario.Key, usuario.Value);
+            }
+        }
+
+        public static void sobre()
+        {
+            limpaTela();
+            mesagemPrincipal("Sobre o Jogo");
+            Console.WriteLine("Desenvolvedor: Ricardo Santana");
+            Console.WriteLine("E-mail: slopes.ricardo@gmail.com");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+![C#](https://i.imgur.com/Zo7uSPN.gif)
+
+  
+## :superhero: Adivinhe o Número
+
+Este repositório contém um pequeno projeto que consiste em um jogo.  Foi proposto no curso ****Python 3 parte 1: Introdução à nova versão da linguagem**** ministrado por [Nico Steppat](https://www.linkedin.com/in/steppat/) na plataforma de cursos [Alura](https://www.alura.com.br/).
+
+  
+
+## :clipboard: Sobre o projeto
+
+O projeto feito durante o treinamento na linguagem Python, porém resolvi fazer uma implementação utilizando a linguagem C#. 
+Não utilizei recursos de POO neste projeto, todo o código  foi estruturado dentro de uma única classe. Tive como objetivo exercitar o conhecimento da sintaxe básica da linguagem e também porque achei divertido fazer desta forma :smiley:.
+Fiz algumas modificações como acrescentar um menu inicial e um registro :writing_hand: contendo o nome e a pontuação do jogador. 
+:grey_exclamation: Não fiz validação das entradas de dados e nem tratamento de exceções. 
+
+## :desktop_computer: Pré-requisitos
+
+Windows 10
+.NET Framework 4.6.1
+
+## :arrow_forward: Como executar o projeto
+
+Abri o terminal no diretório onde está o arquivo Program.cs e digitar:
+`C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe Program.cs `
+
+O caminho onde se encontra o `csc.exe` pode variar como também a versão instalada, consulte a [documentação](https://docs.microsoft.com/pt-br/dotnet/csharp/) para maiores informações :thumbsup:
+
+Um vez executado o comando acima será criado um arquivo `Program.exe` no mesmo diretório.
+
+É só clicar no executável!!!
+
+## :pencil2: Licença
+
+Este projeto esta sobe a licença MIT.
+
+  
+
+Feito com ❤️ por Ricardo Santana
+
+
